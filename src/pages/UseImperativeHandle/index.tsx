9c6a89d0887ebde28a889baed8b07d510eb7196d
yanlele <331393627@qq.com>
2019-09-29 23:02:18 +0800

    feat(page): complete UseImperativeHandle demo

--- src/pages/UseImperativeHandle/index.tsx
+++ src/pages/UseImperativeHandle/index.tsx
@@ -1,7 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, forwardRef, useEffect, useRef } from 'react';
+import Child, { ChildRef } from './Child';
+
+const ChildRefComponent = forwardRef(Child);
 
 const UseImperativeHandle: FC = () => {
-  return <div>hello,</div>;
+  const childRef = useRef<ChildRef>(null);
+
+  useEffect(() => {
+    console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
+    console.log(childRef.current);
+    console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
+  }, []);
+
+  return (
+    <>
+      <ChildRefComponent ref={childRef} parent="name" />
+    </>
+  );
 };
 
 export default UseImperativeHandle;
