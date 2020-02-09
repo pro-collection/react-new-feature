c1b063060027ea69671a656742f12e5ab83161c1
yanlele <331393627@qq.com>
2020-02-09 22:06:37 +0800

    feat: 修改文件

--- src/pages/RouterComponent/index.tsx
+++ src/pages/RouterComponent/index.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import { Button } from 'antd';
 
-const RouterComponent: FC = () => {
-  return <div>index</div>;
+const RouterComponent: FC = props => {
+  console.log(props);
+
+  return (
+    <>
+      <Button type={'default'}>返回首页</Button>
+    </>
+  );
 };
 
 export default RouterComponent;
