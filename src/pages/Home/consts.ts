8128b7074fa206d3361bdec4f4f70a4c69265c54
yanlele <331393627@qq.com>
2020-02-09 21:33:23 +0800

    feat: add pages consts

---
+++ src/pages/Home/consts.ts
@@ -0,0 +1,15 @@
+interface Pages {
+  path: string;
+  title: string;
+}
+
+export const pages: Pages[] = [
+  {
+    path: '/props_child',
+    title: 'api: this.props.children 传值到子组件',
+  },
+  {
+    path: '/use_imperative_handle',
+    title: 'api: UseImperativeHandle',
+  },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,11 @@
 import React, { FC } from 'react';
 import { List } from 'antd';
 import { Link } from 'react-router-dom';
+import { pages } from './consts';
 
 const { Item: ListItem } = List;
 
 const HomeContainer: FC = () => {
-  const pages = [
-    {
-      path: '/props_child',
-      title: 'this.props.children传值到子组件',
-    },
-    {
-      path: '/use_imperative_handle',
-      title: 'UseImperativeHandle',
-    },
-  ];
-
   return (
     <div style={{ marginLeft: '20px' }}>
       <List
