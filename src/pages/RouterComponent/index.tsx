6ebdb3313b9b9dedddc950ac8b3bf66e1b3ab206
yanlele <331393627@qq.com>
2020-02-09 22:18:43 +0800

    feat: modify component

--- src/pages/RouterComponent/index.tsx
+++ src/pages/RouterComponent/index.tsx
@@ -1,12 +1,29 @@
 import React, { FC } from 'react';
 import { Button } from 'antd';
+import { RouteComponentProps } from 'react-router';
+import queryString from 'query-string';
 
-const RouterComponent: FC = props => {
-  console.log(props);
+const RouterComponent: FC<RouteComponentProps> = props => {
+  const handleGoHome = () => props.history.goBack();
+
+  const handlePushParams = () => {
+    props.history.push(
+      `/router-component?${queryString.stringify({
+        name: 'YanLeLe',
+        age: 27,
+        address: 'CQ',
+      })}`,
+    );
+  };
 
   return (
     <>
-      <Button type={'default'}>返回首页</Button>
+      <Button type={'default'} onClick={handleGoHome}>
+        返回首页
+      </Button>
+      <Button type={'default'} onClick={handlePushParams}>
+        推送参数
+      </Button>
     </>
   );
 };
