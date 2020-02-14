05a795d85905282d4408778d2c1310c26a3da844
yanlele <331393627@qq.com>
2020-02-14 22:00:53 +0800

    feat: add options

--- src/pages/OuterClickExample/index.tsx
+++ src/pages/OuterClickExample/index.tsx
@@ -1,9 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 
 const OuterClickExample: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleContainer = useRef(null);
+
+  // useEffect(()=> {
+  //   window.addEventListener('click', )
+  // })
+
+  const onClickHandler = () => setIsOpen(!isOpen);
+
   return (
-    <div>
-      外部点击
+    <div ref={toggleContainer}>
+      <button onClick={onClickHandler}>select an option</button>
+      {isOpen && (
+        <ul>
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Option 3</li>
+        </ul>
+      )}
     </div>
   );
 };
