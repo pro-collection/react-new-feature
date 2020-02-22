d8d72189306b9960559c79d4780d8416748c8570
yanlele <331393627@qq.com>
2020-02-22 22:49:56 +0800

    feat: modify code template

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'codeTemplate.js', 'chore', 'revert']],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
   },
---
+++ mock/codeTemplate.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fse = require('fs-extra');
+
+const basePagePath = path.resolve(__dirname, '../src/pages');
+const pagePaths = fse.readdirSync(basePagePath);
+
+const handleGetPage = pagePathParams =>
+  pagePathParams.reduce((resolvePath, currentPath) => {
+    const currents = fse.readdirSync(path.resolve(basePagePath, currentPath));
+    let result = {};
+    if (currents.some(current => !current.includes('.'))) {
+      result = Object.assign(
+        {},
+        result,
+        handleGetPage(currents.filter(current => !current.includes('.')).map(item => `${currentPath}/${item}`)),
+      );
+    }
+    return Object.assign(
+      resolvePath,
+      {
+        [currentPath]: path.resolve(basePagePath, currentPath, 'index.tsx'),
+      },
+      result,
+    );
+  }, {});
+
+const resultPath = handleGetPage(pagePaths);
+
+const result = Object.entries(resultPath).reduce((result, [key, value]) => {
+  return Object.assign(result, {
+    [key]: { code: fse.readFileSync(value, 'utf-8') },
+  });
+}, {});
+
+module.exports = result;
