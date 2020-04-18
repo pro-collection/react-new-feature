4bbb35cbcfc7d9aa8114cfbda8f81db8031a191e
yanlele <331393627@qq.com>
2020-04-18 18:40:12 +0800

    feat: add profiler

--- src/pages/Profiler/index.tsx
+++ src/pages/Profiler/index.tsx
@@ -1,12 +1,26 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, Profiler as ReactProfiler, ProfilerOnRenderCallback } from 'react';
 
 const Profiler: FC = () => {
   const [value, setValue] = useState('');
 
+  const onRender: ProfilerOnRenderCallback = (id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
+    console.group('info');
+    console.log('id', id);
+    console.log('phase', phase);
+    console.log('actualDuration', actualDuration);
+    console.log('baseDuration', baseDuration);
+    console.log('startTime', startTime);
+    console.log('commitTime', commitTime);
+    console.log('interactions', interactions);
+    console.groupEnd();
+  };
+
   return (
     <div>
-      <input type="text" onChange={event => setValue(event.target.value)} /> <br />
-      <span>{value}</span>
+      <ReactProfiler id="profiler" onRender={onRender}>
+        <input type="text" onChange={event => setValue(event.target.value)} /> <br />
+        <p>输出内容：{value}</p>
+      </ReactProfiler>
     </div>
   );
 };
